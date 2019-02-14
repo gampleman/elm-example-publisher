e14b69e55e51e30772825fa68d4745f8371311d3
Lucas Payr <orasund@gmail.com>
2019-02-14 20:35:24 +0100

    bug fix

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
 var _ = require('lodash'),
-    Promise = require('promise'),
-    glob = Promise.denodeify(require('glob')),
-    elm = require('node-elm-compiler'),
-    path = require('path'),
-    marked = require('marked'),
-    highlight = require('highlight.js'),
-    webshot = Promise.denodeify(require('webshot')),
-    sharp = require('sharp'),
-    fs = (function(fs) {
-      ['readFile', 'stat', 'writeFile'].forEach(function(fun) {
-        fs[fun] = Promise.denodeify(fs[fun]);
-      });
-      return fs;
-    })(require('fs')),
-    fileUrl = require('file-url'),
-    StaticServer = require('static-server'),
-    minify = require('html-minifier').minify,
-    workboxBuild = require('workbox-build');
-    ;
+  Promise = require('promise'),
+  glob = Promise.denodeify(require('glob')),
+  elm = require('node-elm-compiler'),
+  path = require('path'),
+  marked = require('marked'),
+  highlight = require('highlight.js'),
+  webshot = Promise.denodeify(require('webshot')),
+  sharp = require('sharp'),
+  fs = (function (fs) {
+    ['readFile', 'stat', 'writeFile'].forEach(function (fun) {
+      fs[fun] = Promise.denodeify(fs[fun]);
+    });
+    return fs;
+  })(require('fs')),
+  fileUrl = require('file-url'),
+  StaticServer = require('static-server'),
+  minify = require('html-minifier').minify,
+  workboxBuild = require('workbox-build');
+;
 
 var exampleConfigRegexp = /\{\-(\s*{(?:.|\n)*?})\s*\-\}\n*/m;
 
@@ -25,34 +25,34 @@ var exampleConfigRegexp = /\{\-(\s*{(?:.|\n)*?})\s*\-\}\n*/m;
  * Compiles an elm file to a target file location returning a promise when done.
  */
 function compileElm(input, output) {
-  return new Promise(function(resolve, reject) {
-      elm.compile(input, {output: output, optimize: true}).on('close', function(exitCode) {
-        if (exitCode !== 0) {
-          reject(new Error('Compilation failed'));
-        } else {
-          resolve(output);
-        }
-      });
-  })
-  .then(_.partial(fs.readFile, _, 'utf8'))
-  .then(function(src) {
-    return fs.writeFile(output, minify(src, {
-      minifyJS: {
-        mangle: true,
-        compress: {
-          pure_funcs: ["F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9"],
-          pure_getters: true,
-          keep_fargs: false,
-          unsafe_comps: true,
-          unsafe: true,
-          passes:  2
-        }
+  return new Promise(function (resolve, reject) {
+    elm.compile(input, { output: output, optimize: true }).on('close', function (exitCode) {
+      if (exitCode !== 0) {
+        reject(new Error('Compilation failed'));
+      } else {
+        resolve(output);
       }
-    }))
-  });
+    });
+  })
+    .then(_.partial(fs.readFile, _, 'utf8'))
+    .then(function (src) {
+      return fs.writeFile(output, minify(src, {
+        minifyJS: {
+          mangle: true,
+          compress: {
+            pure_funcs: ["F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9"],
+            pure_getters: true,
+            keep_fargs: false,
+            unsafe_comps: true,
+            unsafe: true,
+            passes: 2
+          }
+        }
+      }))
+    });
 }
 
-module.exports = function(options) {
+module.exports = function (options) {
   _.defaults(options, {
     inputDir: process.cwd(),
     outputDir: process.cwd(),
@@ -69,30 +69,30 @@ module.exports = function(options) {
   function loadTemplate(name) {
     var templatePath;
     return fs.stat(path.join(options.templateDir, name))
-    .then(function(stat) {
-      if (stat.isFile()) {
-        return path.join(options.templateDir, name);
-      }
-      console.info("Could not find", name, "in", options.templateDir, "using default instead");
-      return path.join(__dirname, 'templates', name);
-    })
-    .then(_.partial(fs.readFile, _, 'utf8'))
-    .then(function(templateSrc) {
-      var t = _.template(templateSrc, {imports: {_: _}});
-      return function(output, data) {
-        var result = minify(t(data), {
-          collapseWhitespace: true,
-          minifyCSS: true,
-          minifyJS: true,
-          sortAttributes: true,
-          sortClassName: true
-        });
-        return fs.writeFile(path.join(options.outputDir, output), result).then(function(out) {
-          console.log('Succesfully generated ' + path.join(options.outputDir, output));
-          return out;
-        });
-      }
-    });
+      .then(function (stat) {
+        if (stat.isFile()) {
+          return path.join(options.templateDir, name);
+        }
+        console.info("Could not find", name, "in", options.templateDir, "using default instead");
+        return path.join(__dirname, 'templates', name);
+      })
+      .then(_.partial(fs.readFile, _, 'utf8'))
+      .then(function (templateSrc) {
+        var t = _.template(templateSrc, { imports: { _: _ } });
+        return function (output, data) {
+          var result = minify(t(data), {
+            collapseWhitespace: true,
+            minifyCSS: true,
+            minifyJS: true,
+            sortAttributes: true,
+            sortClassName: true
+          });
+          return fs.writeFile(path.join(options.outputDir, output), result).then(function (out) {
+            console.log('Succesfully generated ' + path.join(options.outputDir, output));
+            return out;
+          });
+        }
+      });
   }
 
   function filterFiles(files) {
@@ -105,9 +105,9 @@ module.exports = function(options) {
           return file;
         }
       });
-    })).then(function(f) {
+    })).then(function (f) {
       return f;
-    }).then(_.partial(_.filter, _, _.identity));
+    }).then(_.partial(_.filter, _, _.identity))
       .then(function (f) {
         console.log("Found " + f.length + " Examples");
         return f;
@@ -115,7 +115,7 @@ module.exports = function(options) {
   }
 
   function parseExampleConfig(f) {
-    return fs.readFile(f, 'utf8').then(function(source) {
+    return fs.readFile(f, 'utf8').then(function (source) {
 
       var m = source.match(exampleConfigRegexp);
       var defaults = {
@@ -139,23 +139,23 @@ module.exports = function(options) {
       },
       defaultWhiteBackground: true,
       renderDelay: delay
-    }, additionalOptions.webshot)).then(function() {
+    }, additionalOptions.webshot)).then(function () {
       console.log('Succesfully generated ' + bigPicture);
       var w = Math.floor(options.width / 3),
-          h = Math.floor(options.height / 3)
+        h = Math.floor(options.height / 3)
       var resizer = sharp(bigPicture);
 
-      return Promise.all(_.flatMap([1, 2], function(factor) {
+      return Promise.all(_.flatMap([1, 2], function (factor) {
         var fName = path.join(dir, name + (factor > 1 ? '@' + factor + 'x.png' : '.png'));
         var resized = resizer.clone().resize(w * factor, h * factor)
 
-         return [
+        return [
           resized.clone().webp().toFile(path.join(dir, name + (factor > 1 ? '@' + factor + 'x.webp' : '.webp')))
 
-          ,resized.toFile(fName)
-          .then(function() {
-            console.log('Succesfully generated ' + fName);
-          })]
+          , resized.toFile(fName)
+            .then(function () {
+              console.log('Succesfully generated ' + fName);
+            })]
       }).concat(resizer.clone().webp().toFile(path.join(dir, name + '@3x.webp'))));
     });
   }
@@ -166,26 +166,26 @@ module.exports = function(options) {
       rootPath: options.outputDir,
       port: port
     });
-    return new Promise(function(resolve, reject) {
-      server.start(function() {
-        return Promise.all(files.map(function(file) {
-          return Promise.resolve({filename: file, basename: path.basename(file, '.elm')})
-          .then(function(f) {
-            return compileElm(f.filename, path.join(options.outputDir, f.basename, 'iframe.html'))
-              .then(function(output) {
-                return parseExampleConfig(f.filename).then(function(opts) {
-                  var dir = path.join(options.outputDir, f.basename);
-                  var snaps = ['preview'].concat(opts.additionalShots);
-                  return Promise.all(snaps.map(function(snap) {
-                    var url = 'http://localhost:' + port + "/" + path.join(f.basename, 'iframe.html');
-                    var p = url + (snap === 'preview' ? '' : '#' + snap);
-                    var additionalOptions = opts.options && opts.options[snap] || {}
-                    return snapPicture(dir, snap, p, opts.delay, additionalOptions);
-                  }));
+    return new Promise(function (resolve, reject) {
+      server.start(function () {
+        return Promise.all(files.map(function (file) {
+          return Promise.resolve({ filename: file, basename: path.basename(file, '.elm') })
+            .then(function (f) {
+              return compileElm(f.filename, path.join(options.outputDir, f.basename, 'iframe.html'))
+                .then(function (output) {
+                  return parseExampleConfig(f.filename).then(function (opts) {
+                    var dir = path.join(options.outputDir, f.basename);
+                    var snaps = ['preview'].concat(opts.additionalShots);
+                    return Promise.all(snaps.map(function (snap) {
+                      var url = 'http://localhost:' + port + "/" + path.join(f.basename, 'iframe.html');
+                      var p = url + (snap === 'preview' ? '' : '#' + snap);
+                      var additionalOptions = opts.options && opts.options[snap] || {}
+                      return snapPicture(dir, snap, p, opts.delay, additionalOptions);
+                    }));
+                  });
                 });
-              });
-          });
-        })).then(function(stuff) {
+            });
+        })).then(function (stuff) {
           server.stop();
           resolve(stuff);
         }, reject);
@@ -216,7 +216,7 @@ module.exports = function(options) {
     var elmPackagePath = path.join(currentDir, 'elm.json');
 
     return fs.readFile(path.join(currentDir, 'elm.json'), 'utf8')
-      .catch(function(err) {
+      .catch(function (err) {
         if (err.code === 'ENOENT') {
           return readElmJSON(baseDir, path.dirname(currentDir));
         } else {
@@ -228,7 +228,7 @@ module.exports = function(options) {
 
 
   function processFile(f) {
-    return fs.readFile(f, 'utf8').then(function(source) {
+    return fs.readFile(f, 'utf8').then(function (source) {
       var firstCommentRegexp = /\{\-\|((?:.|\n)*?)\s*\-\}\n+/m;
       var m = source.match(firstCommentRegexp);
       if (m) {
@@ -248,25 +248,25 @@ module.exports = function(options) {
   }
 
   function processDependencies(f) {
-    return processFile(f).then(function(top) {
-      return elm.findAllDependencies(f, [], options.inputDir).then(function(deps) {
-        return deps.map(function(dep) {
+    return processFile(f).then(function (top) {
+      return elm.findAllDependencies(f, [], options.inputDir).then(function (deps) {
+        return deps.map(function (dep) {
           return processFile(path.join(options.inputDir, dep));
         });
-      }).then(function(objs) {
+      }).then(function (objs) {
         return {
           sources: [top].concat(objs),
           iframe: 'iframe.html',
           name: path.basename(f, '.elm'),
-          displayName: path.basename(f, '.elm').replace(/[a-z][A-Z]/g, function(s) {
+          displayName: path.basename(f, '.elm').replace(/[a-z][A-Z]/g, function (s) {
             return s[0] + ' ' + s[1];
           }),
           path: f,
           style: {
-            source: function(name) {
+            source: function (name) {
               return fs.readFileSync(require.resolve('highlight.js/styles/' + name + '.css'), 'utf8')
             },
-            path: function(name) {
+            path: function (name) {
               return require.resolve('highlight.js/styles/' + name + '.css');
             }
           }
@@ -281,32 +281,33 @@ module.exports = function(options) {
       loadTemplate('show.jst'),
       loadTemplate('index.jst'),
       readElmJSON(options.inputDir).then(JSON.parse)
-    ]).then(_.spread(function(elmData, showTemplate, indexTemplate, elmPackage) {
-      return indexTemplate('index.html', {allExamples: elmData, width: options.width, height: options.height, package: elmPackage, custom: options.templateData})
-      .then(function() {
-        return Promise.all(elmData.map(function(ed) {
-          return showTemplate(path.join(ed.name, 'index.html'), _.merge({
-            allExamples: elmData, width: options.width, height: options.height, package: elmPackage, custom: options.templateData}, ed));
-        }))
-      });
+    ]).then(_.spread(function (elmData, showTemplate, indexTemplate, elmPackage) {
+      return indexTemplate('index.html', { allExamples: elmData, width: options.width, height: options.height, package: elmPackage, custom: options.templateData })
+        .then(function () {
+          return Promise.all(elmData.map(function (ed) {
+            return showTemplate(path.join(ed.name, 'index.html'), _.merge({
+              allExamples: elmData, width: options.width, height: options.height, package: elmPackage, custom: options.templateData
+            }, ed));
+          }))
+        });
     }));
   }
 
   return Promise.all([glob(options.inputDir + '/**/*.elm'), glob(options.inputDir + options.exclude)])
-  .then(_.spread(_.difference))
-  .then(filterFiles)
-  .then(function(elmFilesWithMain) {
-    return Promise.all([
-      compileAndRenderPreviews(elmFilesWithMain),
-      renderPages(elmFilesWithMain)
-    ]);
-  })
-  .then(_.spread(function(previews, pages) {
-    return workboxBuild.generateSW({
-      swDest: path.join(options.outputDir, 'service-worker.js'),
-      globDirectory: options.outputDir,
-      globPatterns: ["**/*.{html,css}", "**/preview.png"]
+    .then(_.spread(_.difference))
+    .then(filterFiles)
+    .then(function (elmFilesWithMain) {
+      return Promise.all([
+        compileAndRenderPreviews(elmFilesWithMain),
+        renderPages(elmFilesWithMain)
+      ]);
     })
+    .then(_.spread(function (previews, pages) {
+      return workboxBuild.generateSW({
+        swDest: path.join(options.outputDir, 'service-worker.js'),
+        globDirectory: options.outputDir,
+        globPatterns: ["**/*.{html,css}", "**/preview.png"]
+      })
 
-  }));
+    }));
 };
